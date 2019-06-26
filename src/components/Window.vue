49095c753bcb3590bda2f289be85675f65ac127b
paola_borja <Gborja311@gmail.com>
2019-06-25 23:43:03 -0500

    dimensiones de la ventana---paola borja closed#3

---
+++ src/components/Window.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="stage">
+       <v-stage ref="stage" :config="configKonva">
+
+       </v-stage>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            configKonva:{
+                width:1000,
+                height:500
+            }
+        }
+    }
+}
+</script>
